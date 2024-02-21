34183aefbfc9da4a38217f812035cd1eea25d8c7
Giovana Marsigli <117399277+GiovanaMarsigli@users.noreply.github.com>
2024-02-21 10:52:36 -0300

    Add files via upload

--- EXERCICIOS INTELLIJ/EXERCICIO 5/src/Main.kt
+++ EXERCICIOS INTELLIJ/EXERCICIO 5/src/Main.kt
@@ -1,8 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 package fundamentos
-fun main(args: Array<String>){
-        val aprovados = listOf("João", "Maria", "Pedro")
-        print("O primeiro colocado foi ${aprovados[0]}")
-    }
+fun main(args: Array<String>) {
+    val aprovados = listOf("João", "Maria", "Pedro")
+    print("O primeiro colocado foi ${aprovados[0]}")
 
+
+}
